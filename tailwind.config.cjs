2d3a993f69059a624225d6d2b90bab5bb9dd0763
acm-97 <alejandro97.acm@gmail.com>
2023-01-08 17:51:06 -0500

    initial commit

---
+++ tailwind.config.cjs
@@ -0,0 +1,29 @@
+/** @type {import('tailwindcss').Config} */
+const plugin = require('tailwindcss/plugin');
+
+module.exports = {
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    extend: {},
+    colors: {
+      'bg-gray': '#2a303c',
+      'bg-gray-light': '#3f485a',
+      'bg-gray-dark': '#242933',
+    },
+  },
+  plugins: [
+    require('daisyui'),
+    plugin(function ({ addBase, theme, addVariant }) {
+      // addBase({
+      //   h1: { fontSize: theme('fontSize.7xl') },
+      //   h2: { fontSize: theme('fontSize.6xl') },
+      //   h3: { fontSize: theme('fontSize.3xl') },
+      //   h4: { fontSize: theme('fontSize.2xl') },
+      //   h6: { fontSize: theme('fontSize.lg') },
+      //   h5: { fontSize: theme('fontSize.base') },
+      // });
+      addVariant('hocus', ['&:hover', '&:focus']);
+      addVariant('not-last', '&:not(:last-child)');
+    }),
+  ],
+};
