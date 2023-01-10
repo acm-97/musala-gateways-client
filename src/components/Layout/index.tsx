dcf4aaa3266de9a22491552fc41973e84e54f5a1
acm-97 <alejandro97.acm@gmail.com>
2023-01-10 03:46:19 -0500

    styles: fix th static and layout main

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -8,7 +8,7 @@ import { ConfirmModal } from '@/components';
 const Layout = () => (
   <ReactQueryProvider>
     <ModalProvider>
-      <main className=" min-h-[100vh] w-full px-52">
+      <main className="min-h-[100vh] w-full px-52 max-lg:px-20 max-sm:px-6">
         <div className="mx-auto max-w-[1000px] py-28">
           <Outlet />
         </div>
